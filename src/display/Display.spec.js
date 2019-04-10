94fccc95febf335953b5dc65eaa19acf0ed8d772
t-tullis <t.tullis34@gmail.com>
2019-04-10 14:08:14 -0700

    added some tests

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,14 @@
-// Test away!
+import React from 'react'
+// import renderer from 'react-test-renderer'
+import {render} from 'react-testing-library'
+import Display from './Display';
+
+
+describe('<Display />', () => {
+    it('gate is defaulted to unlocked and open', () => {
+        const {getByText } = render(<Display />)
+
+        getByText('Unlocked')
+        getByText(/open/i)
+    })
+})
